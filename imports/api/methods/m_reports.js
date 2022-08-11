ec77d94ecd2cada0b06fae871b2ffbc542723b84
rktel <rakinakuy@gmail.com>
2022-08-11 14:28:57 -0500

    new

--- imports/api/methods/m_reports.js
+++ imports/api/methods/m_reports.js
@@ -2,16 +2,19 @@ import { Meteor } from 'meteor/meteor';
 import { Reports } from '../collections/c_reports';
 
 Meteor.methods({
-    'reports.query': async function(){
+    'reports.query': async function () {
         const reports = await Reports.find().fetch();
         return reports;
     },
-    'reports.insert': function(report_array){
-        for(const key in report_array){
+    'reports.insert': function (report_array) {
+        for (const key in report_array) {
             const report = Reports.insert(report_array[key]);
         }
     },
-    'reports.getTodayReport': function(deviceIMEI){
-
+    'reports.getTodayReport': function () {
+        let startToday = new Date();
+        startToday.setHours(0, 0, 0, 0);
+        const reports = Reports.find({}, { dateAndTime: { $gte: startToday } });
+        console.log(reports);
     }
 })
