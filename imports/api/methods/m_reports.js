d78a3f0d360ecb74cee0de295c36221d20145286
rktel <rakinakuy@gmail.com>
2022-08-11 15:01:07 -0500

    new

--- imports/api/methods/m_reports.js
+++ imports/api/methods/m_reports.js
@@ -21,7 +21,11 @@ Meteor.methods({
     'reports.getYesterdayReport': function () {
         let startYesterday = new Date();
         startYesterday.setDate(startYesterday.getDate()-1);
-        console.log(startYesterday);
+        startYesterday.setHours(0,0,0,0);
+        let endYesterday = new Date();
+        endYesterday.setDate(endYesterday.getDate()-1);
+        endYesterday.setHours(23,59,59,999);
+        console.log(startYesterday," & " ,endYesterday);
         //const reports = Reports.find({}, { dateAndTime: { $gte: startToday }, sort:{serverTime: -1} }).fetch();
         //return reports;
     }
