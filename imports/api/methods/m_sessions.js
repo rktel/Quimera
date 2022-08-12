4042058fc8ef6ed46e42b2f4d32b906dc6b384c3
rktel <rakinakuy@gmail.com>
2022-08-12 16:29:18 -0500

    new

--- imports/api/methods/m_sessions.js
+++ imports/api/methods/m_sessions.js
@@ -4,9 +4,9 @@ import { Sessions } from '../collections/c_sessions';
 Meteor.methods({
     'sessions.upsert': function(dataObject){
         const {imei,_session,serverTimeFormat} = dataObject;
-        Sessions.upsert(imei, {
+        Sessions.upsert({imei}, {
             $set:{
-                _session,serverTimeFormat,imei
+                _session,serverTimeFormat
             }
         })
     }
