9835e4fd9aae70dbe063631da64c2bf8e6c2ee74
rktel <rakinakuy@gmail.com>
2022-08-15 17:18:00 -0500

    command

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -4,7 +4,9 @@
   import Navbar from "./components/Navbar.svelte";
   import ReportsTable from "./components/ReportsTable.svelte";
 
-  
+  $: {
+    console.log(Meteor.reports);
+  }
 </script>
 
 <Navbar>
