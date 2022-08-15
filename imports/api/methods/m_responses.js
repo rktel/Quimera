64898acc690607f6d131ca09d11de05666609c0a
rktel <rakinakuy@gmail.com>
2022-08-15 17:04:57 -0500

    command

--- imports/api/methods/m_responses.js
+++ imports/api/methods/m_responses.js
@@ -1,8 +1,8 @@
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import { Responses } from "../collections/c_responses";
 
 Meteor.methods({
-    'responses.insert': function(responsesObject){
+    'responses.insert': function (responsesObject) {
         Responses.insert(responsesObject)
     }
 })
--- imports/ui/components/Navbar.svelte
+++ imports/ui/components/Navbar.svelte
@@ -1,5 +1,92 @@
+<script>
+  import { Meteor } from "meteor/meteor";
+  import { fly } from "svelte/transition";
+  import DatabaseEye from "svelte-material-icons/DatabaseEye.svelte";
+  import TimerSandEmpty from "svelte-material-icons/TimerSandEmpty.svelte";
+  import TimerSandFull from "./components/icons/TimerSandFull.svelte";
+  import VisualStudioCode from "./components/icons/VisualStudioCode.svelte";
+  let dateReportSelected = 0;
+  let startDay,endDay;
+  let imei,command;
+  Meteor.reports = [];
+  const jsendCommand = (_) => {
+    Meteor.call('commands.origin',imei,command);
+  }
+  const jgetReports = (_) => {
+
+     switch (dateReportSelected) {
+       case 0:
+         Meteor.call('reports.getTodayReport', imei,(e,r)=> {
+          reports = r;
+          console.log(r);
+        });
+         break;
+       case 1:
+         Meteor.call('reports.getYesterdayReport',imei, (e,r)=> {
+          reports = r;
+          console.log(r);
+        });
+         break;
+       case 2:
+         Meteor.call('reports.getRangeReport',imei, startDay, endDay,(e,r)=> {
+          reports = r;
+          console.log(r);
+        });
+         break;
+       default:
+         break;
+     }
+  };
+  $: {
+    console.log(dateReportSelected);
+  }
+</script>
 <div class="navbar">
-    <slot name="left"></slot>
-    <slot name="center"></slot>
-    <slot name="right"></slot>
+    <div name="left" class="item-container">
+        <select name="dateView" id="dateView" bind:value={dateReportSelected}>
+          <option value={0}>HOY</option>
+          <option value={1}>AYER</option>
+          <option value={2}>FECHA</option>
+        </select>
+        {#if dateReportSelected === 2}
+          <div class="item-container" transition:fly={{ x: -200, duration: 500 }}>
+            <span class="span">
+              <TimerSandEmpty size="1.4em" />
+            </span>
+            <input type="date" name="date-start" id="date-start" bind:value={startDay}/>
+            <span class="span">
+              <TimerSandFull size="1.4em" />
+            </span>
+            <input type="date" name="date-end" id="date-end" bind:value={endDay}/>
+          </div>
+        {/if}
+          <div class="item-container">
+              <input
+              type="text"
+              name="imei"
+              id="imei"
+              placeholder="IMEI"
+              bind:value={imei}
+            />
+            <button class="btn primary" disabled={!imei} on:click={jgetReports}>
+              <DatabaseEye size="1.4em" />
+            </button>
+          </div>
+        {#if reports.length > 0}
+          <div class="item-container" transition:fly={{ x: 0, duration: 300 }}>
+              <input
+              type="text"
+              name="command"
+              id="command"
+              placeholder="Comando"
+              bind:value={command}
+            />
+            <button class="btn secondary" disabled={!command} on:click={jsendCommand}>
+              <VisualStudioCode size="1.4em" />
+            </button>
+          </div>
+        {/if}
+      </div>
+      <!-- <div name="center" class="item-container">George</div>-->
+      <div name="right" class="item-container" />
 </div>
