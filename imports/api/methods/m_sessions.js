9d86924de49698a835aa4ce19e3680f33d53266a
rktel <rakinakuy@gmail.com>
2022-08-17 14:22:33 -0500

    add

--- imports/api/methods/m_sessions.js
+++ imports/api/methods/m_sessions.js
@@ -2,17 +2,17 @@ import { Meteor } from 'meteor/meteor';
 import { Sessions } from '../collections/c_sessions';
 
 Meteor.methods({
-    'sessions.upsert': function(dataObject){
-        const {imei, _session, serverTime, serverTimeFormat, protocolID} = dataObject;
-        Sessions.upsert({imei}, {
-            $set:{
-                _session, serverTime, serverTimeFormat, protocolID
+    'sessions.upsert': function (dataObject) {
+        const { imei, _session, createdTime, createdTimeFormat, protocolID } = dataObject;
+        Sessions.upsert({ imei }, {
+            $set: {
+                _session, createdTime, createdTimeFormat, protocolID
             }
         })
     },
-    'sessions.getSingle': async function(imei){
+    'sessions.getSingle': async function (imei) {
         imei = Number(imei);
-        const session = await Sessions.findOne({imei});
+        const session = await Sessions.findOne({ imei });
         return session;
     }
 });
