7f3e7ce5a689b804e3739487a8035161b2eabb45
rktel <rakinakuy@gmail.com>
2022-08-22 18:48:54 -0500

    login

--- imports/ui/Login.svelte
+++ imports/ui/Login.svelte
@@ -1,39 +1,45 @@
 <script>
-    import { Meteor } from 'meteor/meteor';
-
+    import { fade } from 'svelte/transition';
+    import { Meteor } from "meteor/meteor";
+    
+	let visible = true;
     let username = "";
     let password = "";
 
     const handleSubmit = () => {
         Meteor.loginWithPassword(username, password);
-    }
+    };
 </script>
-
-<form class="login-form" on:submit|preventDefault={handleSubmit}>
-    <div>
-        <label htmlFor="username">Username</label>
-
-        <input
+{#if visible}
+    <form class="login-form" on:submit|preventDefault={handleSubmit} transition:fade>
+        <div>
+            <label htmlFor="username">Username</label>
+    
+            <input
                 type="text"
                 placeholder="Username"
                 name="username"
                 required
                 bind:value={username}
-        />
-    </div>
-
-    <div>
-        <label htmlFor="password">Password</label>
-
-        <input
+            />
+        </div>
+    
+        <div>
+            <label htmlFor="password">Password</label>
+    
+            <input
                 type="password"
                 placeholder="Password"
                 name="password"
                 required
                 bind:value={password}
-        />
-    </div>
-    <div>
-        <button type="submit">Log In</button>
-    </div>
-</form>
+            />
+        </div>
+        <div>
+            <button type="submit">Log In</button>
+        </div>
+    </form>
+{/if}
+
+
+
