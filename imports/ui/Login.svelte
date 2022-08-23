d9171bac20b043947c6912d632c7a3f18e22f617
rktel <rakinakuy@gmail.com>
2022-08-23 12:31:30 -0500

    login

--- imports/ui/Login.svelte
+++ imports/ui/Login.svelte
@@ -1,49 +1,43 @@
 <script>
     import { fly } from "svelte/transition";
     import { Meteor } from "meteor/meteor";
-    
-  
+
     let username = "";
     let password = "";
 
     const jlogin = () => {
         Meteor.loginWithPassword(username, password);
     };
-
- 
 </script>
-    <div class="relative h-screen  flex bg-slate-300">
-        <form  on:submit|preventDefault={jlogin} >
-            <div>
-                <label htmlFor="username">Username</label>
-        
-                <input
-                    type="text"
-                    placeholder="Username"
-                    name="username"
-                    required
-                    bind:value={username}
-                />
-            </div>
-        
-            <div>
-                <label htmlFor="password">Password</label>
-        
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    required
-                    bind:value={password}
-                />
-            </div>
-            <div>
-                <button type="submit">Log In</button>
-            </div>
-        </form>
-    </div>
 
+<div class="relative h-screen flex bg-zinc-400">
+    <div class="hidden md:flex">Peppa</div>
+    <form on:submit|preventDefault={jlogin}>
+        <div>
+            <label htmlFor="username">Username</label>
 
+            <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                required
+                bind:value={username}
+            />
+        </div>
 
+        <div>
+            <label htmlFor="password">Password</label>
 
-
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+                bind:value={password}
+            />
+        </div>
+        <div>
+            <button type="submit">Log In</button>
+        </div>
+    </form>
+</div>
