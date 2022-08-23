f3d44314c0d5f993c4798800f5ddc926d7f1335d
rktel <rakinakuy@gmail.com>
2022-08-22 22:11:12 -0500

    routing

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,14 +1,15 @@
 <script>
   // import { Meteor } from "meteor/meteor";
+  import { Router, Route } from 'svelte-routing';
   import { fly } from "svelte/transition";
   import Navbar from "./components/Navbar.svelte";
   import ReportsTable from "./components/ReportsTable.svelte";
-  import Drawer from "./components/Drawer.svelte";
-  import Login from "../ui/Login.svelte";
 
+  import Login from "../ui/Login.svelte";
+  export let url = "";
   let loggedIn = false;
   let userPermission = "";
-  let visible = false;
+
   $m: {
     loggedIn = !!Meteor.user();
     let user = Meteor.user();
@@ -20,16 +21,21 @@
   setTimeout(setTimer, 5000);
 </script>
 
-{#if !loggedIn}
-  <Login />
-{/if}
-
-{#if visible}
-  {#if loggedIn}
-    <div transition:fly={{ x: 200, duration: 1000 }}>
-      <Navbar />
-      <ReportsTable />
-      <Drawer />
-    </div>
+<Router url={url}>
+  {#if !loggedIn}
+    <main>
+      <Route path="/login" component={Login} />
+    </main>
   {/if}
-{/if}
+  {#if loggedIn}
+  <Navbar />
+    <main>
+      <Route path="/secure/users-home">
+        <ReportsTable />
+      </Route>
+    </main>
+  {/if}
+</Router>
+
+
+
