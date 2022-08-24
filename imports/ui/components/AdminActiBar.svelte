75fea42082a33144016989b50ba26952cf8c685e
rktel <rakinakuy@gmail.com>
2022-08-23 20:05:23 -0500

    login

--- imports/ui/components/AdminActiBar.svelte
+++ imports/ui/components/AdminActiBar.svelte
@@ -1,5 +1,5 @@
 <div class="bg-zinc-200 flex items-center h-10">
-    <div class="text-gray-700 bg-slate-500">Usuarios</div>
+    <div class="text-gray-700 bg-slate-500 h-9 px-3">Usuarios</div>
     <div class="text-gray-700">Reportes</div>
     <div class="text-gray-700">Comandos</div>
 </div>
