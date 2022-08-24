146c02e5ed06331a1b72dac89e581b090662ec6d
rktel <rakinakuy@gmail.com>
2022-08-24 16:54:25 -0500

    new

--- imports/ui/Admin.svelte
+++ imports/ui/Admin.svelte
@@ -11,23 +11,21 @@
     </TabList>
 
     <TabPanel>
-        <div class="flex h-full">
-            <div class="bg-white m-px mt-1 ml-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)] basis-[460px]">1</div>
-            <div class="bg-white m-px mt-1 ml-[2px] mr-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)] flex-1">2</div>
+        <div class="tabPanelContainer">
+            <div class="tabPanelSideLeft">1</div>
+            <div class="tabPanelSideRight">2</div>
         </div>
     </TabPanel>
-
     <TabPanel>
-        <div class="flex h-full">
-            <div class="bg-white m-px mt-1 ml-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)] basis-[460px]">1</div>
-            <div class="bg-white m-px mt-1 ml-[2px] mr-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)] flex-1">2</div>
+        <div class="tabPanelContainer">
+            <div class="tabPanelSideLeft">1</div>
+            <div class="tabPanelSideRight">2</div>
         </div>
     </TabPanel>
-
     <TabPanel>
-        <div class="flex h-full">
-            <div class="bg-white m-px mt-1 ml-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)] basis-[460px]">1</div>
-            <div class="bg-white m-px mt-1 ml-[2px] mr-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)] flex-1">2</div>
+        <div class="tabPanelContainer">
+            <div class="tabPanelSideLeft">1</div>
+            <div class="tabPanelSideRight">2</div>
         </div>
     </TabPanel>
 </Tabs>
