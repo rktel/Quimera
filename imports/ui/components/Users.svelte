b44070c5313715f4ae6bffe3f73be3d5012914e5
rktel <rakinakuy@gmail.com>
2022-08-24 17:26:10 -0500

    new

--- imports/ui/components/Users.svelte
+++ imports/ui/components/Users.svelte
@@ -1,4 +1,11 @@
 <div class="tabPanelContainer">
-    <div class="tabPanelSideLeft">Users</div>
+    <div class="tabPanelSideLeft">
+
+        <div class="p-3 flex flex-col gap-3">
+            <button class="btn btn-light"> CREAR USUARIO </button>
+            <button class="btn btn-light"> VER USUARIOS </button>
+        </div>
+
+    </div>
     <div class="tabPanelSideRight">2</div>
 </div>
