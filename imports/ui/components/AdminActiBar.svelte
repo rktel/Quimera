5c5a19b544a8d044bc1b3a71d5d3c44fc6169348
rktel <rakinakuy@gmail.com>
2022-08-23 23:44:26 -0500

    new

--- imports/ui/components/AdminActiBar.svelte
+++ imports/ui/components/AdminActiBar.svelte
@@ -1,6 +1,6 @@
-<div class="bg-zinc-100 flex items-center h-10 sm:h-8 select-none">
+<div class="flex items-center h-10 sm:h-8 select-none">
     <button class="tab">Usuarios</button>
-    <button class="tab">Reportes</button>
+    <button class="tab tab-active">Reportes</button>
     <button class="tab">Mensajes</button>
     <button class="tab">Ruta</button>
     <button class="tab">Auditoria</button>
