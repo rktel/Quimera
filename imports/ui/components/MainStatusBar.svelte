b12c094946a0d754e3be24fa95636ace7e22fea2
rktel <rakinakuy@gmail.com>
2022-08-23 23:00:51 -0500

    new

--- imports/ui/components/MainStatusBar.svelte
+++ imports/ui/components/MainStatusBar.svelte
@@ -50,7 +50,7 @@
                 focusable="false"
                 data-prefix="fas"
                 data-icon="at"
-                class="text-white w-4 h-4 sm:w-3 sm:h-3"
+                class="text-white w-4 h-4"
                 role="img"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
