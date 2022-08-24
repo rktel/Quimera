ecdec3b03b51154b21e05ecb684eeb4d9ad82062
rktel <rakinakuy@gmail.com>
2022-08-23 20:20:14 -0500

    login

--- imports/ui/components/AdminActiBar.svelte
+++ imports/ui/components/AdminActiBar.svelte
@@ -1,11 +1,11 @@
 <div class="bg-zinc-200 flex gap-1 items-center h-10">
-    <div class="text-gray-700 border border-zinc-300 cursor-pointer rounded shadow-lg h-9 px-10 grid place-content-center">
+    <div class="text-gray-700 border border-zinc-300 cursor-default rounded shadow-lg h-9 px-10 grid place-content-center">
         <span>Usuarios</span>
     </div>
-    <div class="text-gray-700 h-9 px-10 grid cursor-pointer place-content-center rounded hover:bg-zinc-300">
+    <div class="text-gray-700 h-9 px-10 grid cursor-default place-content-center rounded hover:bg-zinc-300">
         <span>Reportes</span>
     </div>
-    <div class="text-gray-700 h-9 px-10 grid  cursor-pointer place-content-center rounded hover:bg-zinc-300">
+    <div class="text-gray-700 h-9 px-10 grid  cursor-default place-content-center rounded hover:bg-zinc-300">
         <span>Comandos</span>
     </div>
 </div>
