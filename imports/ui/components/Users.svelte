dc33262b7070b9f146fa260b6d22eafa84fb3cac
rktel <rakinakuy@gmail.com>
2022-08-24 17:54:34 -0500

    new

--- imports/ui/components/Users.svelte
+++ imports/ui/components/Users.svelte
@@ -2,7 +2,7 @@
     <div class="tabPanelSideLeft">
 
         <div class="p-3 flex flex-col gap-3">
-            <button class="btn btn-light" on:click={jopenModal}> CREAR USUARIO </button>
+            <button class="btn btn-light" on:click={jtoggleModal}> CREAR USUARIO </button>
             <button class="btn btn-light"> VER USUARIOS </button>
         </div>
 
@@ -12,13 +12,15 @@
 
 {#if openModal}
 <div class="absolute z-10 top-0 bottom-0 right-0 left-0 flex justify-center items-center bg-slate-500 opacity-30" >
-    <h1>Peppa Pig</h1>
+    <div class="h-60 w-52 bg-white rounded border border-solid border-zinc-400">
+
+    </div>
   </div>
 {/if}
 
   <script>
     let openModal = false;
-    const jopenModal = () => {
-        openModal = true;
+    const jtoggleModal = () => {
+        openModal = !openModal;
     }
   </script>
