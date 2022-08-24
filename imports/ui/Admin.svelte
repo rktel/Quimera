eec39eb55692a8a5658e00b9ee4353ab4d1db0e6
rktel <rakinakuy@gmail.com>
2022-08-24 16:35:47 -0500

    new

--- imports/ui/Admin.svelte
+++ imports/ui/Admin.svelte
@@ -13,7 +13,7 @@
     <TabPanel>
         <div class="grid grid-cols-2 h-full">
             <div class="bg-white m-px mt-1 ml-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)]">1</div>
-            <div class="bg-white m-px mt-1 ml-1 mr-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)]">2</div>
+            <div class="bg-white m-px mt-1 ml-[2px] mr-1 rounded-sm border border-solid border-zinc-400 h-[calc(100%_-_8px)]">2</div>
         </div>
     </TabPanel>
 
