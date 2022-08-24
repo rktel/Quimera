8b0ded188073f33c2c3c62cd4c7b7f18b309cc4c
rktel <rakinakuy@gmail.com>
2022-08-24 18:27:03 -0500

    new

--- imports/ui/components/Users.svelte
+++ imports/ui/components/Users.svelte
@@ -13,9 +13,9 @@
 {#if openModal}
 <div class="absolute z-10 top-0 bottom-0 right-0 left-0 flex justify-center items-center bg-slate-500 bg-opacity-25" >
     <div class="relative z-20 h-2/3 w-1/3 bg-white rounded border border-solid border-zinc-400">
-        <div class="statusBar rounded">
+        <div class="statusBar rounded-t">
             <div></div>
-            <button class="btn btn-light btn-small" on:click={jtoggleModal}>Cerrar</button>
+            <button class="btn btn-light btn-small" on:click={jtoggleModal}> x Cerrar</button>
         </div>
     </div>
   </div>
