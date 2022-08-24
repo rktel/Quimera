3a9fb8f59371990d379bc7c2109e8e24fe577aa4
rktel <rakinakuy@gmail.com>
2022-08-23 19:47:40 -0500

    login

--- server/main.js
+++ server/main.js
@@ -29,11 +29,3 @@ Meteor.startup(() => {
   }
 });
 
-const user = Meteor.users.findOne({username: SEBAS_USERNAME})
-const _id = user._id;
-
-Meteor.users.update(_id, {$set :{
-  profile: {
-    accountType: 'super'
-  }
-}})
