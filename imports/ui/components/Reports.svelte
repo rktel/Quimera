05d86bf776f54a26e15427582c8a5573937d3027
rktel <rakinakuy@gmail.com>
2022-08-25 01:03:46 -0500

    new

--- imports/ui/components/Reports.svelte
+++ imports/ui/components/Reports.svelte
@@ -1,4 +1,18 @@
 <div class="tabPanelContainer">
-    <div class="tabPanelSideLeft">Reports</div>
+    <div class="tabPanelSideLeft">
+
+        <div class="p-3 flex flex-col gap-3">
+            <select name="dateReportSelect" id="dateView" bind:value={dateReportSelected} class="form-select">
+                <option value={0}>Hoy</option>
+                <option value={1}>Ayer</option>
+                <option value={2}>Fecha</option>
+            </select>
+        </div>
+
+    </div>
     <div class="tabPanelSideRight">2</div>
 </div>
+<script>
+      let dateReportSelected = 0;
+</script>
+
