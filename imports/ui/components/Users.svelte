3d99c3dd9690300b064317a3d7bfc83ecd3d39f3
rktel <rakinakuy@gmail.com>
2022-08-24 21:20:40 -0500

    new

--- imports/ui/components/Users.svelte
+++ imports/ui/components/Users.svelte
@@ -12,14 +12,14 @@
 
 {#if openModal}
 <div class="absolute z-10 top-0 bottom-0 right-0 left-0 flex justify-center items-center bg-slate-500 bg-opacity-25" >
-    <div class="h-2/3 w-1/3 bg-white shadow-lg p-3">
+    <div class="h-2/3 w-1/3 bg-white shadow-lg">
         
         <div class="defaultBar">
             <div></div>
             <button class="btn btn-light btn-small" on:click={jtoggleModal}> X &nbsp;  Cerrar</button>
         </div>
 
-            <form class="flex flex-col items-center gap-5 bg-slate-500 h-[calc(100%_-_32px)]">
+            <form class="flex flex-col justify-center items-center gap-5 bg-slate-500 h-[calc(100%_-_32px)]">
                 <div class="flex flex-col">
                     <label for="username" class="inline-block mb-2 text-gray-700"
                         >Nombre de usuario</label>
