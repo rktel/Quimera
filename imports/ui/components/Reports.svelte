81c052e6039bfa41e6bcd9b00853d6f648ff28dc
rktel <rakinakuy@gmail.com>
2022-08-25 13:52:11 -0500

    new

--- imports/ui/components/Reports.svelte
+++ imports/ui/components/Reports.svelte
@@ -56,6 +56,13 @@
                 </button>
             </div>
 
+            <div class="flex justify-center">
+                <div class="form-check form-switch">
+                  <input class="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                  <label class="form-check-label inline-block text-gray-800" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+                </div>
+            </div>
+
         </div>
 
     </div>
