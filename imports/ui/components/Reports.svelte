f73c427c8e7d7461b61db2373c2f42f81cc78dad
rktel <rakinakuy@gmail.com>
2022-08-25 14:01:36 -0500

    new

--- imports/ui/components/Reports.svelte
+++ imports/ui/components/Reports.svelte
@@ -55,13 +55,18 @@
                     CONSULTAR
                 </button>
             </div>
-
-            <div class="flex justify-center">
-                <div class="form-check form-switch">
-                  <input class="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                  <label class="form-check-label inline-block text-gray-800" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-                </div>
+            <div class="flex flex-col">
+                <label for="raw-data" class="inline-block mb-2 text-gray-700"
+                >Raw data</label>
+                <input
+                class=""
+                type="checkbox"
+                name="raw-data"
+                id="raw-data"
+                bind:value={imei}
+            />
             </div>
+
 
         </div>
 
