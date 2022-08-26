b2486cd19751b996fb463cf41d226529a2c40f36
rktel <rakinakuy@gmail.com>
2022-08-26 11:51:29 -0500

    new

--- imports/ui/components/Reports.svelte
+++ imports/ui/components/Reports.svelte
@@ -67,7 +67,7 @@
         <div class="p-3 flex flex-col gap-3">
             <div class="flex justify-end">
                 <button class="btn btn-small btn-dark"> 
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="text-white h-5 w-5">
                         <path 
                         fill="currentColor"
                         d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/>
