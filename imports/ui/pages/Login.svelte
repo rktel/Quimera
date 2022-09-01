eb3116a6fada987905bfa35d1541659ebf74f04c
rktel <rakinakuy@gmail.com>
2022-09-01 15:44:45 -0500

    new

--- imports/ui/pages/Login.svelte
+++ imports/ui/pages/Login.svelte
@@ -22,13 +22,13 @@
             <h1 class="text-lg">Iniciar sesión</h1>
             <div for="username" class="grid grid-rows-2 gap-2">
                 <label for="username" class="text-gray-700">Nombre de usuario</label>
-                <input type="text" name="username" required>
+                <input class="h-9 ring-1" type="text" name="username" required>
             </div>
             <div for="password" class="grid grid-rows-2 gap-2">
                 <label for="password" class="text-gray-700">Contraseña</label>
-                <input type="password" name="password" required>
+                <input class="h-9" type="password" name="password" required>
             </div>
-            <button type="submit" class="uppercase bg-gray-400 shadow-xl rounded">inicio de sesión</button>
+            <button type="submit" class="uppercase bg-gray-300 shadow-xl rounded">inicio de sesión</button>
         </form>
 
     </div>
