5ec31ca141ceda2b417d94a136720823bf793429
rktel <rakinakuy@gmail.com>
2022-09-02 16:28:35 -0500

    new

--- imports/ui/pages/subpages/Users.svelte
+++ imports/ui/pages/subpages/Users.svelte
@@ -49,7 +49,7 @@
 <!-- CONTAINER_END -->
 
 <!-- DRAWER -->
-<div class="dark:bg-dark-800 h-[calc(100vh_-_32px)] shadow-lg absolute top-8 z-30 w-[400px]">
+<div class="dark:bg-dark-800 h-[calc(100vh_-_32px)] shadow-lg absolute top-8 z-30 w-[200px]">
   BBBBBBBBBBBBBBBBBB
 </div>
 <!-- DRAWER_END -->
