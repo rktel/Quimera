8c734bc5f83fe4091dc44034aadb0b66632f721e
rktel <rakinakuy@gmail.com>
2022-09-02 11:33:57 -0500

    new

--- imports/ui/pages/Administrator.svelte
+++ imports/ui/pages/Administrator.svelte
@@ -12,7 +12,7 @@
     let subpageOn = subpages[0];
     const fLoadSubpage = subpage => subpageOn = subpage;
     const styles = {
-        active: "bg-white text-alfa-500 shadow-lg"
+        active: "bg-white text-alfa-500 shadow-md"
     }
 
  
@@ -20,9 +20,12 @@
 
 <div class="h-8 text-sm font-normal bg-dark-200 text-dark-500 grid grid-cols-[120px,120px,120px,1fr,200px]">
     {#each subpages as $subpage }
-        <button class="{$subpage.index===subpageOn.index?styles.active:''} " on:click="{_=>fLoadSubpage($subpage)}">{$subpage.name}</button>
+        <button class="{$subpage.index===subpageOn.index?styles.active:''}" on:click="{_=>fLoadSubpage($subpage)}">{$subpage.name}</button>
     {/each}
     <div></div>
-    <div class="bg-alfa-800"></div>
+    <div class="bg-white flex justify-end items-center gap-2">
+        <button class="h-4 w-4 rounded-full bg-alfa-500 text-white">R</button>
+        <button>A</button>
+    </div>
 </div>
 <svelte:component this="{subpageOn.component}"></svelte:component>
