ef390b32eded1bfe301ec0970529a26319afff36
rktel <rakinakuy@gmail.com>
2022-09-02 16:02:45 -0500

    new

--- imports/ui/pages/subpages/Users.svelte
+++ imports/ui/pages/subpages/Users.svelte
@@ -1,10 +1,10 @@
 <div class="dark:bg-dark-800 h-[calc(100vh_-_32px)] grid grid-cols-[340px,1fr] gap-1">
     <div class="grid grid-rows-[40px,40px] gap-3 p-5">
         <div>
-            <button class="uppercase bg-alfa-700 shadow text-white text-xs rounded w-full h-8"> Ver Usuarios</button>
+            <button class="bg-alfa-700 shadow text-white text-xs rounded w-full h-8"> Ver usuarios</button>
         </div>
         <div>
-            <button class="uppercase bg-alfa-700 shadow text-white text-xs rounded w-full h-8"> Crear Usuario</button>
+            <button class="bg-alfa-700 shadow text-white text-xs rounded w-full h-8"> Crear usuario</button>
         </div>
     </div>
     <div class="bg-alfa-900">
