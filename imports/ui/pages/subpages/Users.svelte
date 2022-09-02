341184ffbc3a522f929a233e2bc29f9bfb4e144d
rktel <rakinakuy@gmail.com>
2022-09-02 10:48:20 -0500

    new

--- imports/ui/pages/subpages/Users.svelte
+++ imports/ui/pages/subpages/Users.svelte
@@ -1,1 +1,5 @@
-Users
+<span class="font-light"> Peppa light</span>
+<span class="font-normal"> Peppa normal</span>
+<span class="font-medium"> Peppa medium</span>
+<span class="font-semibold"> Peppa semibold</span>
+<span class="font-bold"> Peppa bold</span>
