6a6f95a0483ef84d45237019b54b6317b86786c8
rktel <rakinakuy@gmail.com>
2022-09-02 10:24:27 -0500

    new

--- imports/ui/pages/Administrator.svelte
+++ imports/ui/pages/Administrator.svelte
@@ -5,9 +5,9 @@
     import Messages from "./subpages/Messages.svelte";
     
     const subpages = [
-        {name:"Users", component: Users},
-        {name:"Reports", component: Reports},
-        {name:"Messages", component: Messages},
+        {index:1, name:"Users", component: Users},
+        {index:2, name:"Reports", component: Reports},
+        {index:3, name:"Messages", component: Messages},
     ]
     let subpageOn = subpages[0];
     const fLoadSubpage = subpage => subpageOn = subpage;
@@ -20,7 +20,7 @@
 
 <div>
     {#each subpages as $subpage }
-        <button class="{$subpage===subpageOn?styles.active:''}" on:click="{_=>fLoadSubpage($subpage)}">{$subpage.name}</button>
+        <button class="{$subpage.index===subpageOn.index?styles.active:''}" on:click="{_=>fLoadSubpage($subpage)}">{$subpage.name}</button>
     {/each}
 </div>
 <svelte:component this="{subpageOn.component}"></svelte:component>
