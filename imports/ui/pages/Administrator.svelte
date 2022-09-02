94bb9bf5de0efb58714b92575ee600eaa53f567f
rktel <rakinakuy@gmail.com>
2022-09-02 00:03:59 -0500

    1

--- imports/ui/pages/Administrator.svelte
+++ imports/ui/pages/Administrator.svelte
@@ -1,1 +1,22 @@
-Administrator
+<script>
+    
+    import Users from "./subpages/Users.svelte";
+    import Reports from "./subpages/Reports.svelte";
+    import Messages from "./subpages/Messages.svelte";
+    
+    const subpages = [
+        {name:"Users", component: Users},
+        {name:"Reports", component: Reports},
+        {name:"Messages", component: Messages},
+    ]
+    let subpageOn = subpages[0];
+    const fLoadSubpage = subpage => subpageOn = subpage;
+    const active ="";
+</script>
+
+<div>
+    {#each subpages as $subpage }
+        <button on:click="{_=>fLoadSubpage($subpage)}">{$subpage.name}</button>
+    {/each}
+</div>
+<svelte:component this="{subpageOn.component}"></svelte:component>
