c654d6ba7162d93b04ea838f8e7b8244ca00c528
rktel <rakinakuy@gmail.com>
2022-09-02 15:37:23 -0500

    new

--- imports/ui/pages/subpages/Users.svelte
+++ imports/ui/pages/subpages/Users.svelte
@@ -1,4 +1,4 @@
-<div class="bg-alfa-500 h-[calc(100vh_-_32px)] grid grid-cols-[360px, 1fr]">
+<div class="bg-alfa-500 h-[calc(100vh_-_32px)] grid grid-cols-[360px,1fr]">
     <div class="bg-alfa-700">
         A
     </div>
