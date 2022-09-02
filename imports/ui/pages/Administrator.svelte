6ff5a17b98d644b642f2f180eb88a2c82ab657c0
rktel <rakinakuy@gmail.com>
2022-09-02 11:16:12 -0500

    new

--- imports/ui/pages/Administrator.svelte
+++ imports/ui/pages/Administrator.svelte
@@ -12,13 +12,13 @@
     let subpageOn = subpages[0];
     const fLoadSubpage = subpage => subpageOn = subpage;
     const styles = {
-        active: "bg-dark-100 text-alfa-500 border border-b-0 border-alfa-500"
+        active: "bg-dark-100 text-alfa-500 shadow"
     }
 
  
 </script>
 
-<div class="h-9 text-sm font-normal bg-dark-50 text-dark-800 grid grid-cols-[120px,120px,120px,1fr,200px]">
+<div class="h-8 text-sm font-normal bg-dark-100 text-dark-500 grid grid-cols-[120px,120px,120px,1fr,200px]">
     {#each subpages as $subpage }
         <button class="{$subpage.index===subpageOn.index?styles.active:''}" on:click="{_=>fLoadSubpage($subpage)}">{$subpage.name}</button>
     {/each}
