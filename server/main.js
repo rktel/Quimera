7b514bdd566ad0b5effee5ae89c4f256b2ffc200
rktel <rakinakuy@gmail.com>
2022-09-01 22:52:10 -0500

    1

--- server/main.js
+++ server/main.js
@@ -21,13 +21,13 @@ Meteor.startup(() => {
   }
 });
 
-/*
+
 const user = Meteor.users.findOne({username: SEBAS_USERNAME})
 const _id = user._id;
 
 Meteor.users.update(_id, {$set :{
    profile: {
-    accountType: 'admin'
+    accountType: 'administrator'
   }
  }})
- */
+ 
