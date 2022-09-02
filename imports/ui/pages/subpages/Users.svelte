db60e5e498601e4c3aecfef0b0add6f6f259178f
rktel <rakinakuy@gmail.com>
2022-09-02 15:53:31 -0500

    new

--- imports/ui/pages/subpages/Users.svelte
+++ imports/ui/pages/subpages/Users.svelte
@@ -1,4 +1,4 @@
-<div class="bg-alfa-500 h-[calc(100vh_-_32px)] grid grid-cols-[340px,1fr] gap-1">
+<div class="h-[calc(100vh_-_32px)] grid grid-cols-[340px,1fr] gap-1">
     <div class="grid grid-rows-[40px,40px] gap-3 p-5">
         <div>
             <button class="uppercase bg-alfa-700 shadow text-white text-xs rounded w-full h-8"> Ver Usuarios</button>
