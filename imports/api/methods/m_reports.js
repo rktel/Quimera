d2cee529e937faf1fca0c83d07092a555409675a
rktel <rakinakuy@gmail.com>
2022-09-05 23:08:17 -0500

    now

--- imports/api/methods/m_reports.js
+++ imports/api/methods/m_reports.js
@@ -13,10 +13,10 @@ Meteor.methods({
                 endDay_ = new Date();
 
                 startDay_.setHours(0, 0, 0, 0);
-                startDay_.setHours(startDay_.getHours() - 5);
+                startDay_.setHours(startDay_.getHours() + 5);
 
                 endDay_.setHours(23, 59, 59, 999);
-                endDay_.setHours(endDay_.getHours() - 5);
+                endDay_.setHours(endDay_.getHours() + 5);
                 break;
             case 1: // yesterday
                 startDay_ = new Date();
@@ -26,20 +26,20 @@ Meteor.methods({
                 endDay_.setDate(endDay_.getDate() - 1);
 
                 startDay_.setHours(0, 0, 0, 0);
-                startDay_.setHours(startDay_.getHours() - 5);
+                startDay_.setHours(startDay_.getHours() + 5);
 
                 endDay_.setHours(23, 59, 59, 999);
-                endDay_.setHours(endDay_.getHours() - 5);
+                endDay_.setHours(endDay_.getHours() + 5);
                 break;
             case 2: // range
                 startDay_ = new Date(startDay_);
                 endDay_ = new Date(endDay_);
                 
                 startDay_.setHours(0, 0, 0, 0);
-                startDay_.setHours(startDay_.getHours() - 5);
+                startDay_.setHours(startDay_.getHours() + 5);
 
                 endDay_.setHours(23, 59, 59, 999);
-                endDay_.setHours(endDay_.getHours() - 5);
+                endDay_.setHours(endDay_.getHours() + 5);
                 break;
             default:
                 break;
