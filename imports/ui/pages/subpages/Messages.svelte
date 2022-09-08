2459167e59527434aaae0cb58bf88e2de5d6b69b
rktel <rakinakuy@gmail.com>
2022-09-08 12:05:23 -0500

    now

--- imports/ui/pages/subpages/Messages.svelte
+++ imports/ui/pages/subpages/Messages.svelte
@@ -128,7 +128,7 @@
 
             <form class="flex items-center gap-5 h-[50px] resize" on:submit|preventDefault={fSendMessage}>
 
-                <input required bind:value={singleCommand} type="text" class="h-9 w-[280px] shadow-lg ring-1 ring-dark-300 rounded-2xl px-3 dark:text-dark-800 focus:outline-none focus:ring-alfa-500">
+                <input required bind:value={singleCommand} type="text" class="h-9 w-[220px] shadow-lg ring-1 ring-dark-300 rounded-2xl px-3 dark:text-dark-800 focus:outline-none focus:ring-alfa-500">
                 <button type="submit" disabled={!singleCommand} class="h-9 w-9 rounded-full bg-white shadow-lg grid place-content-center">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-5 fill-alfa-700 dark:fill-dark-100"><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L277.3 424.9l-40.1 74.5c-5.2 9.7-16.3 14.6-27 11.9S192 499 192 488V392c0-5.3 1.8-10.5 5.1-14.7L362.4 164.7c2.5-7.1-6.5-14.3-13-8.4L170.4 318.2l-32 28.9 0 0c-9.2 8.3-22.3 10.6-33.8 5.8l-85-35.4C8.4 312.8 .8 302.2 .1 290s5.5-23.7 16.1-29.8l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>
                 </button>
