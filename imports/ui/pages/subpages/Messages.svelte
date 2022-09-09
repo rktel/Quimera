41c12588209570784ea1bbcf4a60d8d6d9bf17fd
rktel <rakinakuy@gmail.com>
2022-09-09 18:00:40 -0500

    now

--- imports/ui/pages/subpages/Messages.svelte
+++ imports/ui/pages/subpages/Messages.svelte
@@ -61,7 +61,7 @@
     }
 </script>
 <!-- CONTAINER -->
-<div class="flex sm:h-[calc(100%_-_40px)] h-[calc(100%_-_99px)]">
+<div class="flex sm:h-[calc(100%_-_40px)] h-[calc(100%_-_99px)] dark:bg-dark-700 bg-dark-50">
     <!-- LEFT PANEL -->
     {#if bLeftPanel}
         <div class="grid grid-rows-[50px,1fr] shadow-lg basis-[300px] text-xs">
@@ -110,66 +110,73 @@
     {/if}
     <!-- LEFT PANEL_END -->
 
-    <!-- RIGHT PANEL -->
-    <div class="bg-beta-900 text-dark-800 h-[calc(100%_-_1px)] flex-1  flex flex-col overflow-x-hidden">
-        <!-- tools -->
-        <div class="bg-alfa-400 h-[46px] flex justify-start sm:justify-end px-5">
-            <div class="grid place-content-center">
-                <button class="h-7 w-7 rounded-md bg-white shadow-lg grid place-content-center" on:click={fTogglePanelLeft}>
-                    {#if bLeftPanel}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-5 fill-alfa-700 dark:fill-dark-100"><path d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/></svg>
-                    {/if}
-                    {#if !bLeftPanel}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-5 fill-alfa-700 dark:fill-dark-100"><path d="M456 224H312c-13.3 0-24-10.7-24-24V56c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l40 40L442.3 5.7C446 2 450.9 0 456 0s10 2 13.7 5.7l36.7 36.7C510 46 512 50.9 512 56s-2 10-5.7 13.7L433 143l40 40c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8zm0 64c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-40 40 73.4 73.4c3.6 3.6 5.7 8.5 5.7 13.7s-2 10-5.7 13.7l-36.7 36.7C466 510 461.1 512 456 512s-10-2-13.7-5.7L369 433l-40 40c-6.9 6.9-17.2 8.9-26.2 5.2s-14.8-12.5-14.8-22.2V312c0-13.3 10.7-24 24-24H456zm-256 0c13.3 0 24 10.7 24 24V456c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-40-40L69.7 506.3C66 510 61.1 512 56 512s-10-2-13.7-5.7L5.7 469.7C2 466 0 461.1 0 456s2-10 5.7-13.7L79 369 39 329c-6.9-6.9-8.9-17.2-5.2-26.2s12.5-14.8 22.2-14.8H200zM56 224c-9.7 0-18.5-5.8-22.2-14.8s-1.7-19.3 5.2-26.2l40-40L5.7 69.7C2 66 0 61.1 0 56s2-10 5.7-13.7L42.3 5.7C46 2 50.9 0 56 0s10 2 13.7 5.7L143 79l40-40c6.9-6.9 17.2-8.9 26.2-5.2s14.8 12.5 14.8 22.2V200c0 13.3-10.7 24-24 24H56z"/></svg>
-                    {/if}
-                </button>
-            </div>
-        </div>
-        <!-- tools_end -->
-        <!-- messages -->
-        <div class="bg-special flex-1 flex p-5">
-            <!-- responses -->
-            <div class=" basis-1/2 overflow-hidden flex flex-col py-3">
-                <!-- singleResponse -->
-                <div class="h-[50px] flex mb-2">
-                    <div class="flex flex-col gap-1 bg-beta-200 justify-center px-3 rounded-xl overflow-auto">
-                        <p class="text-xs font-bold whitespace-nowrap text-dark-600">Lorem, beatae, sequi, quia reiciendis?</p>
-                        <p style="font-size: 10px;" class="text-dark-500">12 ago. 2022 - 14:22:24</p>
-                    </div>
+    {#if bPanelRight}
+        <!-- RIGHT PANEL -->
+        <div class="bg-beta-900 text-dark-800 h-[calc(100%_-_1px)] flex-1  flex flex-col overflow-x-hidden">
+            <!-- tools -->
+            <div class="bg-alfa-600 h-[46px] flex justify-start sm:justify-end px-5">
+                <div class="grid place-content-center">
+                    <button class="h-7 w-7 rounded-md bg-white shadow-lg grid place-content-center" on:click={fTogglePanelLeft}>
+                        {#if bLeftPanel}
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-5 fill-alfa-700 dark:fill-dark-100"><path d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/></svg>
+                        {/if}
+                        {#if !bLeftPanel}
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-5 fill-alfa-700 dark:fill-dark-100"><path d="M456 224H312c-13.3 0-24-10.7-24-24V56c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l40 40L442.3 5.7C446 2 450.9 0 456 0s10 2 13.7 5.7l36.7 36.7C510 46 512 50.9 512 56s-2 10-5.7 13.7L433 143l40 40c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8zm0 64c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-40 40 73.4 73.4c3.6 3.6 5.7 8.5 5.7 13.7s-2 10-5.7 13.7l-36.7 36.7C466 510 461.1 512 456 512s-10-2-13.7-5.7L369 433l-40 40c-6.9 6.9-17.2 8.9-26.2 5.2s-14.8-12.5-14.8-22.2V312c0-13.3 10.7-24 24-24H456zm-256 0c13.3 0 24 10.7 24 24V456c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-40-40L69.7 506.3C66 510 61.1 512 56 512s-10-2-13.7-5.7L5.7 469.7C2 466 0 461.1 0 456s2-10 5.7-13.7L79 369 39 329c-6.9-6.9-8.9-17.2-5.2-26.2s12.5-14.8 22.2-14.8H200zM56 224c-9.7 0-18.5-5.8-22.2-14.8s-1.7-19.3 5.2-26.2l40-40L5.7 69.7C2 66 0 61.1 0 56s2-10 5.7-13.7L42.3 5.7C46 2 50.9 0 56 0s10 2 13.7 5.7L143 79l40-40c6.9-6.9 17.2-8.9 26.2-5.2s14.8 12.5 14.8 22.2V200c0 13.3-10.7 24-24 24H56z"/></svg>
+                        {/if}
+                    </button>
                 </div>
-                <!-- singleResponse_end -->
             </div>
-            <!-- responses_end -->
-
-            <!-- commands -->
-            <div class=" basis-1/2 overflow-hidden flex flex-col py-3">
-
-                <!-- singleCommands -->
-                <div class="h-[50px]  flex justify-end mb-2">
-                    <div class="flex flex-col gap-1 bg-alfa-200 justify-center px-3 rounded-xl overflow-auto">
-                        <p class="text-xs font-bold whitespace-nowrap text-dark-600">Lorem, ipsum recusandae error, beatae, sequi, quia reiciendis?</p>
-                        <p style="font-size: 10px;" class="text-right text-dark-500">12 ago. 2022 - 14:22:24</p>
-                    </div>
+            <!-- tools_end -->
+            <!-- messages -->
+            <div class="bg-special flex-1 flex p-5">
+                <!-- responses -->
+                <div class=" basis-3/5 overflow-hidden flex flex-col py-3">
+                    <!-- arrayResponse -->
+                    {#if responses[0]}
+                        {#each responses as { response, receivedTime }}
+                            <div class="h-[50px] flex mb-2">
+                                <div class="flex flex-col gap-1 bg-beta-200 justify-center px-3 rounded-xl overflow-auto">
+                                    <p class="text-xs font-bold whitespace-nowrap text-dark-600">{response}</p>
+                                    <p style="font-size: 10px;" class="text-dark-500">{formatChat(receivedTime)}</p>
+                                </div>
+                            </div>
+                        {/each}
+                    {/if}
+                    <!-- arrayResponse_end -->
                 </div>
-                <!-- singleCommands_end -->
+                <!-- responses_end -->
 
-
+                <!-- commands -->
+                <div class=" basis-2/5 overflow-hidden flex flex-col py-3">
+                    <!-- arrayCommands -->
+                    {#if commands[0]}
+                        {#each commands as {commandText, createdTime} }
+                            <div class="h-[50px]  flex justify-end mb-2">
+                                <div class="flex flex-col gap-1 bg-alfa-200 justify-center px-3 rounded-xl overflow-auto">
+                                    <p class="text-xs font-bold whitespace-nowrap text-dark-600">{commandText}</p>
+                                    <p style="font-size: 10px;" class="text-right text-dark-500">{formatChat(createdTime)}</p>
+                                </div>
+                            </div>
+                        {/each}
+                    {/if}
+                    <!-- arrayCommands_end -->
+                </div>
+                <!-- commands_end -->
             </div>
-            <!-- commands_end -->
-        </div>
-        <!-- messages_end -->
-        <!-- input action -->
-        <div class="bg-alfa-400 h-[60px] flex px-5">
-            <div class=" flex-1 flex items-center">
-                <input type="text" class="h-[40px] text-dark-700 focus:outline-none px-3 rounded-l-xl flex-1">
-                <button class="bg-dark-50 h-[40px] w-[40px]  text-dark-700 grid place-content-center rounded-r-xl">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-5 fill-dark-500 dark:fill-dark-100"> <path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>
-                </button>
+            <!-- messages_end -->
+            <!-- input action -->
+            <div class="bg-alfa-600 h-[60px] flex px-5">
+                <form class=" flex-1 flex items-center" on:submit|preventDefault={fSendMessage}>
+                    <input bind:value={singleCommand} type="text" class="h-[40px] text-dark-700 focus:outline-none px-3 rounded-l-xl flex-1 text-sm">
+                    <button type="submit" class="bg-dark-50 h-[40px] w-[40px]  text-dark-700 grid place-content-center rounded-r-xl">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-5 fill-dark-500 dark:fill-dark-100"> <path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>
+                    </button>
+                </form>
             </div>
+            <!-- input action_end -->
         </div>
-        <!-- input action_end -->
-    </div>
-    <!-- RIGHT PANEL_END -->
+        <!-- RIGHT PANEL_END -->
+    {/if}
 </div>
 <!-- CONTAINER_END -->
 <style>
