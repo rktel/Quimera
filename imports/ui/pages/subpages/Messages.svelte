5ec3f29ab080c4a88bdf11ca71202d3be29d6239
rktel <rakinakuy@gmail.com>
2022-09-08 22:00:17 -0500

    now

--- imports/ui/pages/subpages/Messages.svelte
+++ imports/ui/pages/subpages/Messages.svelte
@@ -80,9 +80,11 @@
         </div>
         <!-- input action -->
         <div class="bg-beta-200 h-[60px] flex px-5">
-            <div class="bg-beta-400 flex items-center">
-                <input type="text" class="h-[40px]">
-                <button class="bg-white h-[40px] text-dark-700">X</button>
+            <div class="bg-beta-400 flex-1 flex items-center">
+                <input type="text" class="h-[40px] text-dark-700 focus:outline-none px-3 rounded-l-xl flex-1">
+                <button class="bg-dark-50 h-[40px] w-[40px]  text-dark-700 grid place-content-center rounded-r-xl">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-5 fill-beta-700 dark:fill-dark-100"> <path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>
+                </button>
             </div>
         </div>
         <!-- input action_end -->
