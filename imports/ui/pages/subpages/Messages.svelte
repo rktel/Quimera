bad294f5d12df805f3811effe49608a24b74f270
rktel <rakinakuy@gmail.com>
2022-09-09 17:26:28 -0500

    now

--- imports/ui/pages/subpages/Messages.svelte
+++ imports/ui/pages/subpages/Messages.svelte
@@ -113,7 +113,7 @@
     <!-- RIGHT PANEL -->
     <div class="bg-beta-900 text-dark-800 h-[calc(100%_-_1px)] flex-1  flex flex-col">
         <!-- tools -->
-        <div class="bg-alfa-400 h-[46px] flex justify-end sm:justify-start px-5">
+        <div class="bg-alfa-400 h-[46px] flex justify-end px-5">
             <div class="grid place-content-center">
                 <button class="h-7 w-7 rounded-md bg-white shadow-lg grid place-content-center" on:click={fTogglePanelLeft}>
                     {#if bLeftPanel}
