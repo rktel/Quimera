a1b06dd06347bb78004f16a4f472002539eade5c
rktel <rakinakuy@gmail.com>
2022-09-16 16:17:46 -0500

    now

--- imports/api/methods/m_reports.js
+++ imports/api/methods/m_reports.js
@@ -134,7 +134,7 @@ function headersRefactor(_headers) {
       }
       let header = {
         label: _headers[index],
-        state: _headers[index] === 'raw'?false: true,
+        state: true,
         type,
       }
       headers.push(header)
