02557563593058899f1ce0519beaec134c0c061c
rktel <rakinakuy@gmail.com>
2022-11-17 19:55:38 -0500

    now

--- imports/ui/pages/subpages/RealTime.svelte
+++ imports/ui/pages/subpages/RealTime.svelte
@@ -1,7 +1,10 @@
 <script>
     import { udpStreamer } from '../../../api/streamers'
+    let _data = ''
     udpStreamer.on('data', function(data){
+        _data = data
         console.log('Data from server UDP:', data);
     })
 </script>
 <h1>Real Time</h1>
+<h2>{_data}</h2>
